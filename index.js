e916b97552d4d7f92e04ca10db3f6178dad3fc9c
The Sharp Ninja <ninja@thesharp.ninja>
2022-05-27 23:26:45 +0000

    Formal logging methods.

--- index.js
+++ index.js
@@ -4,22 +4,38 @@ const fs = require('fs');
 const https = require('https');
 const shell = "powershell";
 
+let debug = false;
+
+function log(toLog) {
+    console.log('[Install Scoop][info]', toLog);
+}
+
+function err(toLog) {
+    console.error('[Install Scoop]', toLog);
+}
+
+function debug(toLog) {
+    if (debug) {
+        console.log('[Install Scoop][debug]', toLog);
+    }
+}
+
 try {
     // File URL
     const url = 'https://get.scoop.sh';
 
-    const debug = core.getInput('debug') === true;
+    debug = core.getInput('debug') === true;
     const asAdmin = core.getInput('allow-install-as-admin');
     const ext = core.getInput('extension').trim('.');
     const scriptName = `.\\install-scoop.${ext}`;
 
     const executeWith = ext === 'ps1' ? shell : ext === 'bat' ? 'cmd' : '/bin/bash';
 
+    debug("Debug mode enabled.");
+
     // Download the file
     https.get(url, (res) => {
-        if (debug) {
-            console.log(res)
-        }
+        debug(res)
 
         // Open file in local filesystem
         const file = fs.createWriteStream(scriptName);
@@ -32,19 +48,17 @@ try {
             file.close((err) => {
                 if (err) { console.error('error', err); }
                 else {
-                    if (debug) {
-                        console.log(fs.readFileSync(scriptName))
-                    }
+                    debug(fs.readFileSync(scriptName))
                     const { execSync } = require('child_process');
 
                     const command = `${executeWith} ${scriptName} ${asAdmin ? ' -RunAsAdmin' : ''}`;
 
-                    console.log(`Executing: ${command}`);
+                    log(`Executing: ${command}`);
 
                     const stdout = execSync(command);
 
-                    console.log(`stdout.byteLength: ${stdout.byteLength}`);
-                    console.log(`stdout: ${stdout.toString()}`);
+                    log(`stdout.byteLength: ${stdout.byteLength}`);
+                    log(`stdout: ${stdout.toString()}`);
                 }
             });
         });
