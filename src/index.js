2dd8dd9594bf2e9c7da86eb03bd15b98a9d80866
The Sharp Ninja <ninja@thesharp.ninja>
2022-05-28 00:21:00 +0000

    f

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 "use strict";
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from '@actions/core'; 
+import * as github from '@actions/github';
 const fs = require('fs');
-const https = require('follow-redirects').https;
+import * as fs from 'fs';
+import { https } from 'follow-redirects'
+
 const shell = "powershell";
 let _debug = false;
+
 function log(toLog) {
     console.log('[Install Scoop][info]', toLog);
 }
@@ -16,48 +19,51 @@ function dbg(...toLog) {
         console.log('[Install Scoop][debug]', ...toLog);
     }
 }
-try {
-    // File URL
-    const url = 'https://get.scoop.sh';
-    _debug = core.getInput('debug') === "true";
-    log(`core.getInput('debug'): ${core.getInput('debug')}`);
-    log(`_debug: ${_debug}`);
-    const asAdmin = core.getInput('allow-install-as-admin');
-    const ext = core.getInput('extension').trim('.');
-    const scriptName = `.\\install-scoop.${ext}`;
-    const executeWith = ext === 'ps1' ? shell : ext === 'bat' ? 'cmd' : '/bin/bash';
-    dbg("Debug mode enabled.");
-    // Download the file
-    https.get(url, (res) => {
-        const statusCode = res.statusCode;
-        const statusMsg = res.statusMessage;
-        dbg(statusCode, statusMsg);
-        // Open file in local filesystem
-        const file = fs.createWriteStream(scriptName);
-        // Write data into local file
-        res.pipe(file);
-        // Close the file
-        file.on('finish', () => {
-            file.close((err) => {
-                if (err) {
-                    console.error('error', err);
-                }
-                else {
-                    const scr = fs.readFileSync(scriptName).toString();
-                    dbg(scr === null || scr === void 0 ? void 0 : scr.length, scr);
-                    const { execSync } = require('child_process');
-                    const command = `${executeWith} ${scriptName} ${asAdmin ? ' -RunAsAdmin' : ''}`;
-                    log(`Executing: ${command}`);
-                    const stdout = execSync(command);
-                    log(`stdout.byteLength: ${stdout.byteLength}`);
-                    log(`stdout: ${stdout.toString()}`);
-                }
+
+async function run() {
+    try {
+        // File URL
+        const url = 'https://get.scoop.sh';
+        _debug = core.getInput('debug') === "true";
+        log(`core.getInput('debug'): ${core.getInput('debug')}`);
+        log(`_debug: ${_debug}`);
+        const asAdmin = core.getInput('allow-install-as-admin');
+        const ext = core.getInput('extension').trim('.');
+        const scriptName = `.\\install-scoop.${ext}`;
+        const executeWith = ext === 'ps1' ? shell : ext === 'bat' ? 'cmd' : '/bin/bash';
+        dbg("Debug mode enabled.");
+        // Download the file
+        https.get(url, (res) => {
+            const statusCode = res.statusCode;
+            const statusMsg = res.statusMessage;
+            dbg(statusCode, statusMsg);
+            // Open file in local filesystem
+            const file = fs.createWriteStream(scriptName);
+            // Write data into local file
+            res.pipe(file);
+            // Close the file
+            file.on('finish', () => {
+                file.close((err) => {
+                    if (err) {
+                        console.error('error', err);
+                    }
+                    else {
+                        const scr = fs.readFileSync(scriptName).toString();
+                        dbg(scr === null || scr === void 0 ? void 0 : scr.length, scr);
+                        const { execSync } = require('child_process');
+                        const command = `${executeWith} ${scriptName} ${asAdmin ? ' -RunAsAdmin' : ''}`;
+                        log(`Executing: ${command}`);
+                        const stdout = execSync(command);
+                        log(`stdout.byteLength: ${stdout.byteLength}`);
+                        log(`stdout: ${stdout.toString()}`);
+                    }
+                });
             });
+        }).on("error", (err) => {
+            err("Error: ", err.message);
         });
-    }).on("error", (err) => {
-        console.error("Error: ", err.message);
-    });
-}
-catch (e) {
-    console.error(e);
+    }
+    catch (e) {
+        err(e);
+    }
 }
